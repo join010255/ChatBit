5a4ecfa6394df4364d968eb6a9689d34c3b6839b
jony111M <pennyc2Stealer@proton.me>
2026-08-22 15:31:59 +0100

    update

--- backend/src/sockets/presence.socket.js
+++ backend/src/sockets/presence.socket.js
@@ -2,5 +2,53 @@ import User from "../models/user.model.js";
 
 
 const presenceSocket = (io, socket) => {
-    const  
+    const setOnline = async() => {
+        try {
+            await User.update(
+                {
+                    is_online : true
+                
+                },
+                {
+                    where : {
+                        id : socket.user.id
+                    } 
+                }
+            );
+
+            io.emit("presence:update", {
+                userId : socket.user.id,
+                isOnline : true
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const setOffline = async() => {
+        try{
+            await User.update(
+                {
+                    is_online : false
+                },
+                {
+                    where : {
+                        id : socket.user.id
+                    }
+                }
+            )
+
+            io.emit("presence:update", {
+                userId : socket.user.id,
+                isOnline : true
+            })
+        }catch(error){
+            console.error(error)
+        }
+    }
+
+    setOnline()
+    io.on("diconnect", setOffline())
 }
+
+export default presenceSocket;
